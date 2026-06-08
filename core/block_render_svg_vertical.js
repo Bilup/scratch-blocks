e550a4f6fe7ae52416d08caca7ed26b03f490203
DLGrass <2649771611@qq.com>
2026-06-08 20:23:51 +0800

    fix: finally fix issues

--- core/block_render_svg_vertical.js
+++ core/block_render_svg_vertical.js
@@ -541,6 +541,7 @@ Blockly.BlockSvg.prototype.updateColour = function() {
   for (var i = 0, input; input = this.inputList[i]; i++) {
     if (input.outlinePath) {
       if (this.getColourSecondary() == '#4C4C4C') {
+        
         input.outlinePath.setAttribute('fill', this.getColourSecondary());
       }else{
         input.outlinePath.setAttribute('fill', this.getColourTertiary());
